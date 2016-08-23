f340a430fd75a0a185d435f08c394ddf8e7feb30
Nguyen Trong Tin <nguyentrongtin2331@gmail.com>
2016-08-23 14:43:40 +0700

    add file

---
+++ final-parameter.go
@@ -0,0 +1,10 @@
+SamplingFrequency Fs = 44100 Hz
+
+FrameLength L = Fs/2 = 22050 samples per frame
+
+StepSizes = [Pi/2, Pi/6, Pi/10, Pi/14, Pi/18]
+
+BIT_REPEAT = 5
+
+// All results in
+EmbeddedBitsPerSecond = 1600/BIT_REPEAT = 320 bits/sec = 40 characters/second
